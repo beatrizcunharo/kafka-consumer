808719aada847f8f61893f620974f213728858ec
beatrizcunharo <beatrizcunharo@gmail.com>
2021-09-15 15:58:01 -0300

    project: Modificando projeto para integração com o producer

--- src/main/java/consumer/kafkaconsumer/service/KafkaService.java
+++ src/main/java/consumer/kafkaconsumer/service/KafkaService.java
@@ -1,13 +1,28 @@
 package consumer.kafkaconsumer.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+@Slf4j
+@RequiredArgsConstructor
 @Service
 public class KafkaService {
 
-    @KafkaListener(topics = "example", groupId = "group_id")
-    public void consume(String message){
-        System.out.println("Mensagem: "+ message);
+    @Value("${topic.name.consumer")
+    private String topicName;
+
+    @KafkaListener(topics = "${topic.name.consumer}", groupId = "group_id")
+    public void consume(ConsumerRecord<String, String> payload){
+        log.info("Tópico: {}", topicName);
+        log.info("key: {}", payload.key());
+        log.info("Headers: {}", payload.headers());
+        log.info("Partion: {}", payload.partition());
+        log.info("Order: {}", payload.value());
+
     }
+
 }
